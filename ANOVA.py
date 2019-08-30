7f8ec04a2f3c9b237531af42b624c82332bef865
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-08-30 13:28:29 +0200

    Update ANOVA.py

--- ANOVA.py
+++ ANOVA.py
@@ -1,56 +1,45 @@
+''' Seperates dataframe into multiple by treatment
+E.g. if treatment is 'gender' with possible values 1 (male) or 2 (female) 
+the function returns a list of two frames (one with all males the other with all females) '''
+
+def seperated_dataframes(df, treatment):
+    treat_col = data[treatment] # col with the treatment
+    dframes_sep = [] # list to hold seperated dataframes 
+    for cat in categories(treat_col): # Go through all categories of the treatment
+         for the treatmet into a new dataframe
+        df = data[treat_col == cat] # select all rows that match the category        
+        dframes_sep.append(df) # append the selected dataframe
+    return dframes_sep
+
+import numpy as np
+import scipy.stats as scs
+import pandas as pd
+
 def one_way_anova(df, colls_to_test, treatment):
-    #Function that performs a one way anover on all specified values for the specified treatment
-    #Input: df: dataframe the operation is to be run on
-    #colls_to_test: list of strings of collum names for which we are going to perform the anova
-    #treament: string with treatment collum name
-    #output: dataframe containing pvalue and fstat for all collums
-    #The resulting dataframe has the collum names as rows and fstat and pval as collums
-    
-    #First, split the dataset by the treatment
-    dframes_sep = seperated_dataframes(df, treatment)
-    #Init an empty dictionary which will contain the pvalues
+    dframes_sep = seperated_dataframes(df, treatment) # split the dataset by the treatment
     pv_dict = {}
-    #Init empty dictionary which will contain f statistics
     fstat_dict = {}
-    #For every collum we want to run our anova on
-    for collum in colls_to_test:
-        #init empty array which is going to hold the colllum of interest from the seperated dataframes
-        seperated_colls = []
-        #for each of our previously seperated dataframes
-        for df in dframes_sep:
-            #obtain the collum of interest less all empty cells and add it to the list of all collums of interest
-            seperated_colls.append(df.dropna()[collum])
-        #run the one way anova for the sperated collums
-        #usually the function f_oneway takes the different collums comma seperated
-        #Like f_oneway(coll1, coll2, coll3)
-        #The star tells it to treat the list we are passing as such comma seprated variables
-        fstat, pval = scs.f_oneway(*seperated_colls)
-        #save p value and fstat for this collum to the dict
+    for collum in colls_to_test: #For every col we want to run our anova on
+        seperated_colls = []        
+        for df in dframes_sep: # for each of our previously seperated dataframes ...
+            seperated_colls.append(df.dropna()[collum]) # ... obtain the col of interest less all empty cells and add it to the list  
+        fstat, pval = scs.f_oneway(*seperated_colls) # * tells it to treat the list we are passing as such csv
         pv_dict[collum]=pval
         fstat_dict[collum] = fstat
-    #Now we are going to turn those dictionaries into a dataframe
-    #We want the collums in the dataframe to have usefull names
-    #the convention we use is treatment_fstat or treatment_pval
-    #that way we know what we have in there when we look at the dataframe later
-    #first we generate the name of the fstat collum by connecting the treatment name with the string'_fstat'
+
     fstat_coll_name = treatment+'_fstat'
-    #then we convert the fstat dict into a dataframe
     fstatdf = pd.DataFrame.from_dict(fstat_dict, orient='index')
-    #and then we change the collum name in that dataframe
     fstatdf.columns = [fstat_coll_name]
-    #same goes for pvals
+
     pval_coll_name = treatment+'_pval'
     pvdf = pd.DataFrame.from_dict(pv_dict, orient='index')
     pvdf.columns = [pval_coll_name]
-    #then we connect the pval dataframe and the fstat dataframe
+    
     result_dataframe = pd.concat([pvdf,fstatdf],axis=1)
-    #and return the result
     return result_dataframe
 
-interesting_collums = ['loyalty', 'satisfaction','educ', 'gender', 'age', 'Involvement', 'Emotional',
-       'Calculative', 'Trust', 'Image']
+interesting_collums = ['loyalty', 'satisfaction','educ', 'gender', 'age']
 res = one_way_anova(data, interesting_collums, 'loyalty')
-
 
 all_data_frames = []
 for collum in interesting_collums:
@@ -58,14 +47,12 @@ for collum in interesting_collums:
     all_data_frames.append(res)
 
 final = pd.concat(all_data_frames, axis=1)
-final.to_excel('/Users/jannes/Google Drive/ABM/data/all_oneway_anova.xls')
+final.to_excel('/Users/.../data/all_oneway_anova.xls')
 
 
 
 ########################################################
 # Two way anova
-# TODO: Extend for unbalanced design
-# I could not find a good module implementation so this is an implementation from scratch
 # Via: http://www.marsja.se/three-ways-to-carry-out-2-way-anova-with-python/
 # Still not perfect so handle with care
 
