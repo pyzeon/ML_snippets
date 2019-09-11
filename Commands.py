4eeb5d36ebb86dad31d184844359590219582f02
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-09-11 23:02:30 +0200

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1,1 +1,19 @@
 
+help(len)
+len? # help on an object
+
+L = [1, 2, 3]
+L?
+
+square?? # reading the source code
+
+L.<TAB> # see a list of all available attributes of an object
+L.c<TAB>
+from itertools import co<TAB>
+import <TAB> # see which imports are available
+
+str.*find*? # looking for a string method that contains the word find somewhere
+
+%run myscript.py # running external code
+
+%timeit L = [n ** 2 for n in range(1000)]
