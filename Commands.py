a7198b2d22e8b5220ff41d7488aa3c72ea5980c4
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-10-01 10:24:58 +0200

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -18,3 +18,19 @@ str.*find*? # looking for a string method that contains the word find somewhere
 %run myscript.py # running external code
 
 %timeit L = [n ** 2 for n in range(1000)]
+
+
+
+# -----------------------------------------------------------------------------------
+
+       with zipfile.ZipFile(self.fname) as zf:
+            with zf.open(zf.namelist()[0]) as infile:
+                header = infile.readline()
+                datestr, record_count = header.split(b':')
+                self.month = int(datestr[2:4])
+                self.day = int(datestr[4:6])
+                self.year = int(datestr[6:10])
+                utc_base_time = datetime(self.year, self.month, self.day)
+                self.base_time = timezone('US/Eastern').\
+                                                localize(utc_base_time).\
+                                                timestamp()
