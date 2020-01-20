986a429777303ab30e97f0e35c27f3bee36d9cdc
kalmar <oanufriyev@gmail.com>
2020-01-20 01:00:00 +0100

    d

--- snips_analysis.py
+++ snips_analysis.py
@@ -135,6 +135,24 @@ bob.isnull().sum()
     	print(item)
 
 
+
+
+	n = input()
+	s = input()
+	up    = len([i for i in ['1', '2', '3'] if i in s]) ==  0 # true if s does not contains one of these
+	right = len([i for i in ['3', '6', '9', '0'] if i in s]) == 0
+	left  = len([i for i in ['1', '4', '7', '0'] if i in s]) == 0
+	down  = len([i for i in ['7', '9', '0'] if i in s]) == 0
+	ok = not(up or right or left or down) #if the is at least one true then it is not uniqe and print NO
+											#if all of them are 0 then not 0 == true and print YES
+	if ok:
+		print("YES")
+	else: #not ok
+		print("NO")
+
+
+
+
 # Check if arrays are equal
 	A = np.random.randint(0,2,5), B = np.random.randint(0,2,5)
 	equal = np.allclose(A,B) # Assuming identical shape of the arrays and a tolerance for the comparison of values 
--- snips_db.py
+++ snips_db.py
@@ -43,6 +43,36 @@ def get_list_tickers_in_dir(directory=None):
     os.chdir(start_dir)
     return (TCKRS, listdir) 
 
+
+# ----------------------------------------------------------------------------------
+# Merge all csv files of the same struc in the same folder
+import glob
+import pandas as pd
+from time import strftime
+
+def folder_csv_merge(file_prefix, folder_path='', memory='no'):
+    """
+    file_prefix: if you want to add a prefix to the name of final merged file
+    folder_path: no need to declare it. string copied from file explorer to the folder where the files are
+    """
+    if folder_path == '':
+        folder_path = input('Please enter the path where the CSV files are:\n')
+    folder_path = folder_path.replace("\\","/")
+    if folder_path[:-1] != "/":
+        folder_path = folder_path + "/"
+
+    file_list = glob.glob(folder_path + '*.csv')
+
+    combined = pd.concat( [ pd.read_csv(f) for f in file_list ] )
+    if memory == 'no':
+        combined.to_csv(folder_path + 
+                        'combined_{}_{}.csv'.format(file_prefix, 
+                                            strftime("%Y%m%d-%H%M%S")), 
+                        index=False)
+    else:
+        return combined
+    print('done')
+
 #--------------------------------------------------------------------------------------------------------
 # read txt
 
